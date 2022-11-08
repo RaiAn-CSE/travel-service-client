ad9820cb29277a1af1b3e23ef9a5e6237d3a8e87
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-11-09 02:27:55 +0600

    review page added

--- src/Pages/Home/Services/LocationDetails/LocationDetails.js
+++ src/Pages/Home/Services/LocationDetails/LocationDetails.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import Review from '../Review/Review';
 
 const LocationDetails = () => {
     const { title, price, img } = useLoaderData();
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <div className="card card-compact w-full bg-base-100 shadow-xl mb-4">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
+            </div>
+            <div className='text-center p-3'>
+                <Link to='/review'>
+                    <button className="btn btn-primary">Review</button>
+                </Link>
             </div>
         </div>
     );
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import LocationDetails from "../../Pages/Home/Services/LocationDetails/LocationDetails";
+import Review from "../../Pages/Home/Services/Review/Review";
 import ServiceDetails from "../../Pages/Home/Services/ServiceDetails/ServiceDetails";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -25,12 +26,16 @@ const router = createBrowserRouter([
             },
             {
                 path: "/servicedetails",
-                element: <ServiceDetails></ServiceDetails>,
+                element: <ServiceDetails></ServiceDetails>
             },
             {
                 path: "/locationdetails/:id",
                 element: <LocationDetails></LocationDetails>,
                 loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: "/review",
+                element: <Review></Review>
             },
             // {
             //     path: "/checkout/:id",
