8c232111cc6b78652cca91f08455e47fecf4dc23
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-11-10 17:26:20 +0600

    google map added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import useTitle from '../../../hooks/useTitle';
 import Banner from '../Banner/Banner';
+import AboutMe from './AboutMe/AboutMe';
+import GoogleMap from './GoogleMap/GoogleMap';
 import Services from './Services/Services';
 
 
@@ -15,6 +17,13 @@ const Home = () => {
                 <Link to="/services">
                     <button className="btn btn-primary">See All Services</button>
                 </Link>
+            </div>
+            <div>
+                <AboutMe></AboutMe>
+            </div>
+            <div className='flex flex-col items-center my-5'>
+                <h2 className='text-2xl font-semibold mb-4 underline underline-offset-4'>My Location: Daffodil International University</h2>
+                <GoogleMap></GoogleMap>
             </div>
         </div>
     );
